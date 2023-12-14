4769e84bce316631a07fc6c9772cfd4e796cc17f
NagyBotondGaor <nagybotondg@gmail.com>
2023-12-14 13:32:00 +0100

    Hahaha

--- example.py
+++ example.py
@@ -3,8 +3,16 @@ import random
 print("Számok gyakorisága egy adott intervallumban")
 print("mindjárt kicsengetnek,ki nem szarja le most ezt???")
 
-def isPerfect():
-    pass
+def isPerfect(n):
+    if n == 1 or n == 0:
+        return False
+    else:
+            divNumbers = [1]
+    for i in range(2, n, 1):
+        if n % i == 0:
+            divNumbers.append(i)
+    
+    return sum(divNumbers) == n
 
 
 def randomGenerator(s, e, a):
@@ -16,15 +24,18 @@ def randomGenerator(s, e, a):
 
 
 def makeNumber(text):
-    #isCorrect = False
-    while True: #not isCorrect:
+    while True: 
         n = input(text)
         try:
             n = int(n)
-            #isCorrect = True
             return n
         except ValueError:
             print("Agyhalooott...")
+
+#Itt indul a főprogram(main)
+
+perfectNumbers = list()
+perfectNumFreq = dict()
 
 startMessage = "Kezdő érték: "
 lastMessage  = "Végérték: "
@@ -36,4 +47,25 @@ amount =makeNumber(amountMessage)
 
 print(start, end, amount)
 
-randomGenerator(start, end, amount)
+randomNumbers = randomGenerator(start, end, amount)
+print(randomNumbers)
+
+for num in randomNumbers:
+    if isPerfect(num):
+        perfectNumbers.append(num)
+for num in perfectNumbers:
+    if num in perfectNumFreq.keys():
+        perfectNumFreq[num] += 1
+    else:
+        perfectNumFreq[num] = 1
+for key in perfectNumFreq.keys():
+    print(f"{key}: {perfectNumFreq[key]} db")
+
+
+
+
+
+
+
+
+
